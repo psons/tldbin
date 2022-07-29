f2aedd786a1b86ebd511929b3b96b7c01fdfda60
Paul Sons <psons@psons@comcast.net>
2022-07-29 18:46:01 -0500

    set PYTHONPATH in run_tests.sh so it works without special environmet

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,11 @@
 # if this doesn't work, tyy activating the virtalenv.
+set -x
+
 app_home=$(cd $(dirname $0)/..;pwd)
 echo In: $(basename $0)
 cd ${app_home}/tlog/test
+
+export PYTHONPATH="${app_home}/tlog/tlog"
+
 mkdir -p ../unit-test-tmp-dir
 python -m unittest test*.py
